a6e1e326d89a17d83bcedd39e4a36c7e99edab11
tianshiyang <tianshiyang@icourt.cc>
2023-04-11 23:04:19 +0800

    feat: 改为手动架子啊

--- src/main.ts
+++ src/main.ts
@@ -7,32 +7,44 @@ import 'element-plus/dist/index.css'
 import zhCn from "element-plus/lib/locale/lang/zh-cn"
 import actions from "@/qiankun/actions"
 
-import { registerMicroApps, start } from 'qiankun'
+import { loadMicroApp, start, MicroApp } from 'qiankun'
 
 createApp(App)
   .use(store)
   .use(ElementPlus, { locale: zhCn })
   .use(router).mount('#app')
 
-registerMicroApps([
-  {
-    name: 'qiankun-vue2', // app name registered
-    entry: '//localhost:4000',
-    container: '#sub-app',
-    activeRule: '/vue2',
-    props: {
-      router, actions
+let activeMicroApp: MicroApp | null = null
+// 子应用加载逻辑
+router.beforeEach((to, from, next) => {
+  if (to.path.startsWith("/vue3")) {
+    if (activeMicroApp) {
+      activeMicroApp.unmount()
     }
-  },
-  {
-    name: 'qiankun-vue3',
-    entry: '//localhost:5000',
-    container: '#sub-app',
-    activeRule: '/vue3',
-    props: {
-      router, actions
-    }
+    activeMicroApp = loadMicroApp({
+      name: 'qiankun-vue3',
+      entry: '//localhost:5000',
+      container: '#sub-app',
+      props: {
+        router, actions
+      }
+    })
   }
-])
+
+  if (to.path.startsWith("/vue2")) {
+    if (activeMicroApp) {
+      activeMicroApp.unmount()
+    }
+    activeMicroApp = loadMicroApp({
+      name: 'qiankun-vue2', // app name registered
+      entry: '//localhost:4000',
+      container: '#sub-app',
+      props: {
+        router, actions
+      }
+    })
+  }
+  next()
+})
 
 start()
