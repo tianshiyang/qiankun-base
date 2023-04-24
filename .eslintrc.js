2f5689048a911f8a21962683250258f5093c830e
tianshiyang <tianshiyang@icourt.cc>
2023-04-25 00:14:55 +0800

    feat: 配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     "no-unused-expressions": "off",
     "vue/multi-word-component-names": 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "@typescript-eslint/no-explicit-any": ["off"]
   }
 }
